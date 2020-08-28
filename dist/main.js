82e59c0c93977d5b04312947939ab331bddbe458
Mtolentin <mattca701@gmail.com>
2020-08-27 17:13:49 -0700

    Main Menu Foundations

--- dist/main.js
+++ dist/main.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return EventListener_Revolution; });\nclass EventListener_Revolution {\n  constructor(canvas){\n    this.ctx = canvas.getContext(\"2d\");\n    this.dimensions = { width: canvas.width, height: canvas.height };\n  }\n}\n\n//# sourceURL=webpack:///./src/game.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return EventListener_Revolution; });\n/* harmony import */ var _nirvana__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nirvana */ \"./src/nirvana.js\");\n/* harmony import */ var _nirvana__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_nirvana__WEBPACK_IMPORTED_MODULE_0__);\n\n\nclass EventListener_Revolution {\n  constructor(canvas){\n    this.ctx = canvas.getContext(\"2d\");\n    this.dimensions = { width: canvas.width, height: canvas.height };\n    this.ctx.fillStyle = \"white\";\n    this.ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);\n    this.displayMenu.bind(this);\n    this.displayMenu();\n    \n  }\n\n  getMousePos(canvas, event) {\n    var rect = canvas.getBoundingClientRect();\n    return {\n      x: event.clientX - rect.left,\n      y: event.clientY - rect.top\n    };\n  }\n\n  //Function to check whether a point is inside a rectangle\n  isInside(pos, rect) {\n    return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y\n  }\n  \n  displayMenu() {\n    this.ctx.beginPath();\n    this.ctx.rect(250, 350, 200, 100);\n    this.ctx.fillStyle = '#FFFFFF';\n    this.ctx.fillStyle = 'green';\n    this.ctx.fillRect(25, 72, 32, 32);\n    this.ctx.fill();\n    this.ctx.lineWidth = 2;\n    this.ctx.strokeStyle = '#000000';\n    this.ctx.stroke();\n    this.ctx.closePath();\n    this.ctx.font = '40pt Kremlin Pro Web';\n    this.ctx.fillStyle = '#000000';\n    this.ctx.fillText('Start', 345, 415);\n    \n    var canvas = document.getElementById('dance-game');\n    var context = canvas.getContext('2d');\n    \n    debugger\n    //The rectangle should have x,y,width,height properties\n    let rect = {\n      x: 250,\n      y: 350,\n      width: 200,\n      height: 100\n    };\n  \n    //Binding the click event on the canvas\n    canvas.addEventListener('click', function (evt) {\n      let mousePos = getMousePos(canvas, evt);\n    \n      if (isInside(mousePos, rect)) {\n          alert('clicked inside rect');\n        } else {\n          alert('clicked outside rect');\n        }\n    }, false);\n  }\n  \n\n\n}\n\n//# sourceURL=webpack:///./src/game.js?");
 
 /***/ }),
 
@@ -107,6 +107,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _game__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./game */ \"./src/game.js\");\n\n\nconst canvas = document.getElementById('dance-game');\nnew _game__WEBPACK_IMPORTED_MODULE_0__[\"default\"](canvas);\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/nirvana.js":
+/*!************************!*\
+  !*** ./src/nirvana.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// export default class Level {\n//   constructor(dimensions) {\n//     this.dimensions = dimensions;\n//   }\n\n//   drawBackground(ctx) {\n//     ctx.fillStyle = \"skyblue\";\n//     ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);\n//   }\n// }\n\n//# sourceURL=webpack:///./src/nirvana.js?");
 
 /***/ })
 
--- src/game.js
+++ src/game.js
@@ -1,6 +1,68 @@
+import Nirvana from "./nirvana";
+
 export default class EventListener_Revolution {
   constructor(canvas){
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);
+    this.displayMenu.bind(this);
+    this.displayMenu();
+    
   }
+
+  getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    };
+  }
+
+  //Function to check whether a point is inside a rectangle
+  isInside(pos, rect) {
+    return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
+  }
+  
+  displayMenu() {
+    this.ctx.beginPath();
+    this.ctx.rect(250, 350, 200, 100);
+    this.ctx.fillStyle = '#FFFFFF';
+    this.ctx.fillStyle = 'green';
+    this.ctx.fillRect(25, 72, 32, 32);
+    this.ctx.fill();
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = '#000000';
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.font = '40pt Kremlin Pro Web';
+    this.ctx.fillStyle = '#000000';
+    this.ctx.fillText('Start', 345, 415);
+    
+    var canvas = document.getElementById('dance-game');
+    var context = canvas.getContext('2d');
+    
+    debugger
+    //The rectangle should have x,y,width,height properties
+    let rect = {
+      x: 250,
+      y: 350,
+      width: 200,
+      height: 100
+    };
+  
+    //Binding the click event on the canvas
+    canvas.addEventListener('click', function (evt) {
+      let mousePos = getMousePos(canvas, evt);
+    
+      if (isInside(mousePos, rect)) {
+          alert('clicked inside rect');
+        } else {
+          alert('clicked outside rect');
+        }
+    }, false);
+  }
+  
+
+
 }
