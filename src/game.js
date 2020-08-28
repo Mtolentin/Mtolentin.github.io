53a03ab3524bd269e7e92683b1f0fe33d7a53d89
Mtolentin <mattca701@gmail.com>
2020-08-28 12:27:34 -0700

    Splash Foundations

--- src/game.js
+++ src/game.js
@@ -11,18 +11,9 @@ export default class EventListener_Revolution {
     
   }
 
-  getMousePos(canvas, event) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: event.clientX - rect.left,
-      y: event.clientY - rect.top
-    };
-  }
 
   //Function to check whether a point is inside a rectangle
-  isInside(pos, rect) {
-    return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
-  }
+
   
   displayMenu() {
     this.ctx.beginPath();
@@ -40,26 +31,34 @@ export default class EventListener_Revolution {
     this.ctx.fillText('Start', 345, 415);
     
     var canvas = document.getElementById('dance-game');
-    var context = canvas.getContext('2d');
     
     debugger
     //The rectangle should have x,y,width,height properties
-    let rect = {
-      x: 250,
-      y: 350,
-      width: 200,
-      height: 100
-    };
-  
+    
     //Binding the click event on the canvas
     canvas.addEventListener('click', function (evt) {
-      let mousePos = getMousePos(canvas, evt);
-    
+      // let rect = {
+      //   x: 250,
+      //   y: 350,
+      //   width: 200,
+      //   height: 100
+      // };
+      debugger
+      const rect = this.getBoundingClientRect();
+      const mousePos = {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+      };
+      // isInside(pos, rect) {
+      //   return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
+      // }
       if (isInside(mousePos, rect)) {
-          alert('clicked inside rect');
-        } else {
-          alert('clicked outside rect');
-        }
+        // alert('clicked inside rect');
+        console.log("Inside");
+      } else {
+        // alert('clicked outside rect');
+        console.log("Outside");
+      }
     }, false);
   }
   
