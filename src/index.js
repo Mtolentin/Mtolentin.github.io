020cdd4c0ba7f4a49c32e9a1271498cd2ffd670a
Mtolentin <mattca701@gmail.com>
2020-09-11 01:09:04 -0700

    menu transitions

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import EventListener_Revolution from './game';
 
 let parentDiv = document.getElementById("theCanvas");
 const canvas = document.getElementById('dance-game');
-const fXStart = document.getElementById("fXSelect");
+
+let fXSelect = document.createElement("audio");
+fXSelect.id = "fXSelect";
+fXSelect.src = "./dist/assets/sounds/select.ogg";
+fXSelect.load();
+parentDiv.appendChild(fXSelect);
+parentDiv.insertBefore(fXSelect, canvas);
+fXSelect.addEventListener("ended", function () {
+    fXSelect.load();
+});
 
 let introText1 = document.createElement("div");
-introText1.className = "theIntro1";
+introText1.id = "theIntro1";
 introText1.innerText = "eventListener Revolution";
 
 let introText2 = document.createElement("div");
-introText2.className = "theIntro2";
+introText2.id = "theIntro2";
 introText2.innerText = "click to begin";
 
 let game = new EventListener_Revolution(canvas);
@@ -17,48 +26,119 @@ let game = new EventListener_Revolution(canvas);
 setTimeout(function () { 
     parentDiv.appendChild(introText1);
     parentDiv.insertBefore(introText1, canvas);
-
 }, 1500);
 
 setTimeout(function () {
+    let initialClick = canvas.addEventListener('click', goToSongList);
     parentDiv.appendChild(introText2);
     parentDiv.insertBefore(introText2, canvas);
-    canvas.addEventListener('click', initButtonTest);
+}, 4500);
 
-}, 6000);
-
-fXStart.addEventListener("ended", function () {
-    fXStart.load();
-});
-function initButtonTest(evt) {
-    {
-        fXStart.play();
-        const offset = canvas.getBoundingClientRect();
-        let rect = {
-            x: 250 + offset.x,
-            y: 350 + offset.y,
-            width: 200,
-            height: 100
-        };
-        const pos = {
-            x: evt.clientX,
-            y: evt.clientY
-        };
-        
-
-        if (pos.x > rect.x &&
-            pos.x < rect.x + rect.width &&
-            pos.y < rect.y + rect.height &&
-            pos.y > rect.y) {
-
-
-            game.play(canvas, initButtonTest, "testSong");
-
-        } else {
-            console.log("Outside");
-        }
-    }
+function goToSongList(evt) {
+    fXSelect.play();
+    screenFade();
+    setTimeout( function() {
+        introText1.remove();
+        introText2.remove();
+        let bgSongList = document.createElement("img");
+        bgSongList.id = "menuBackground"
+        bgSongList.src = "./dist/assets/gui/bg.png";
+        bgSongList.alt = "Error loading SongList background";
+        parentDiv.appendChild(bgSongList);
+        parentDiv.insertBefore(bgSongList, canvas);
+        let loadSongs = bgSongList.addEventListener("animationend", function() {
+            document.getElementById("fader").remove();
+            displaySongList();
+        });    
+    }, 2000);
 }
+
+function screenFade() {
+    let bgFade = document.createElement("div");
+    bgFade.id = "fader";
+    parentDiv.appendChild(bgFade);
+    parentDiv.insertBefore(bgFade, canvas);
+}
+
+function displaySongList() {
+
+    let nirvana = document.createElement("img");
+    let danzaKaduro = document.createElement("img");
+    let cebuana = document.createElement("img");
+
+
+
+    nirvana.src = "./dist/assets/gui/banners/nirvana.png";
+    danzaKaduro.src = "./dist/assets/gui/banners/danzaKaduro.png";
+    cebuana.src = "./dist/assets/gui/banners/bamBamBam.png";
+
+    nirvana.className= "trackImg";
+    danzaKaduro.className = "trackImg";
+    cebuana.className = "trackImg";
+
+    nirvana.id= "trackNirvana";
+    danzaKaduro.id = "trackDanzaKaduro";
+    cebuana.id = "trackCebuana";
+
+    parentDiv.appendChild(nirvana);
+    parentDiv.insertBefore(nirvana, canvas);
+
+}
+
+
+
+
+
+
+
+
+
+// Testing Codes
+
+{/* <script>
+var currentPlayer;
+function EvalSound(soundobj) {
+
+ var thissound=document.getElementById(soundobj);
+ if(currentPlayer  && currentPlayer != thissound) {
+  currentPlayer.pause(); 
+ }
+ if (thissound.paused)
+        thissound.play();
+    else
+    thissound.pause();
+    thissound.currentTime = 0;
+     currentPlayer = thissound;
+}
+</script> */}
+
+// function initButtonTest(evt) {
+//     fXSelect.play();
+//     const offset = canvas.getBoundingClientRect();
+//     let rect = {
+//         x: 250 + offset.x,
+//         y: 350 + offset.y,
+//         width: 200,
+//         height: 100
+//     };
+//     const pos = {
+//         x: evt.clientX,
+//         y: evt.clientY
+//     };
+    
+
+//     if (pos.x > rect.x &&
+//         pos.x < rect.x + rect.width &&
+//         pos.y < rect.y + rect.height &&
+//         pos.y > rect.y) {
+
+
+//         game.play(canvas, initButtonTest, "testSong");
+
+//     } else {
+//         console.log("Outside");
+//     }
+// }
 
 
 
